58faae53c5d8c69bd1797d032fb878ee969a1417
minascasiou <minas_casiou@hotmail.com>
2024-08-29 01:29:35 +1000

    r

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,4 @@
-using BlazorBasic;
+using Maloo;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -9,7 +9,3 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
-
-//
-
-//v
